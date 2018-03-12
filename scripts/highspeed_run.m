120f547c7ccaddbddacb1fffd6795d61a9afdfc6
Lennart Wittkuhn <wittkuhn@mpib-berlin.mpg.de>
2018-03-12 12:54:58 +0100

    Tried to solve the memory problem
    using clearvars -global

--- scripts/highspeed_run.m
+++ scripts/highspeed_run.m
@@ -2,6 +2,6 @@
 % Lennart Wittkuhn, Independent Max Planck Research Group NeuroCode, 2018
 % Max Planck Institute for Human Development, Berlin, Germany
 % Contact: wittkuhn@mpib-berlin.mpg.de
-close all; clear variables; clc; % clear all variables from the workspace and close all windows
+close all; clear variables; clc; clearvars -global; % clear all variables from the workspace and close all windows
 [Sets,Data,Basics,Parameters,Sounds] = highspeed_settings; % get the highspeed task settings
 [Sets,Data,Basics,Parameters] = highspeed_main(Sets,Data,Basics,Parameters,Sounds); % run the highspeed task main file
